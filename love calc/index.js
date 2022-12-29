29b92b75f6e8453294ef943a30a8cc0bb45c7a47
emekadefirst <119001445+emekadefirst@users.noreply.github.com>
2022-12-29 14:16:49 +0100

    Add files via upload
    It's simple and uses basic prompts . blah blah blah 
    whats your suggestion

---
+++ love calc/index.js
@@ -0,0 +1,26 @@
+// let add = document.getElementById("calc")
+
+// function additon() {
+//     console.log("Clicked")
+// // }
+
+prompt("What is your first name ")
+prompt("What is your second name ")
+
+var loveScore = Math.random() * 100
+loveScore = Math.floor(loveScore) +1;
+
+if(loveScore > 70){
+    alert("Your loveScore is " + loveScore + "%. You both love yourself like the way GOD loves Love Man");
+}
+
+if(loveScore > 30 && loveScore <= 70) {
+    alert("Your loveScore is" + loveScore + "%.");
+}
+if(loveScore <= 30){
+    alert("Your loveScore is" + lovescore + "% you both love yourself like oil and water");
+}
+
+else {
+    alert(" loveScore is " + loveScore + "%.");
+}
